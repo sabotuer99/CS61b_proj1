0dd87153d703f84d695f419ad3db2cb0ed3e4ede
troy <sabotuer99@gmail.com>
2015-08-27 23:40:50 -0600

    Additional implementation on NGramMap

--- Project1/src/ngordnet/test/NGramMapTest.java
+++ Project1/src/ngordnet/test/NGramMapTest.java
@@ -1,14 +1,20 @@
 package ngordnet.test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 import ngordnet.troy.NGramMap;
+import ngordnet.troy.TimeSeries;
 import ngordnet.troy.WordNet;
+import ngordnet.troy.YearlyRecord;
 
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class NGramMapTest {
-	//@Ignore
+	@Ignore
 	@Test
 	public void filesShouldLoadInUnder25Seconds(){
 		//Arrange
@@ -22,5 +28,158 @@ public class NGramMapTest {
 		//Assert
 		System.out.println("Elapsed time: " + time);
 		assertTrue(time < 25000);
+	}
+	
+	@Test
+	public void smallFilesShouldLoadSuperFast(){
+		//Arrange
+		
+		//Act
+		Stopwatch sw = new Stopwatch();
+		@SuppressWarnings("unused")
+		NGramMap ng = new NGramMap("/ngordnet/data/ngrams/very_short.csv", "/ngordnet/data/ngrams/total_counts.csv");
+		long time = (long)(sw.elapsedTime() * 1000);
+		
+		//Assert
+		//System.out.println("Elapsed time: " + time);
+		assertTrue(time < 1000);
+	}
+	
+	@Test
+	public void countInYear_NoDataForYear_ReturnZero(){
+		//Arrange
+		NGramMap sut = new NGramMap("/ngordnet/data/ngrams/very_short.csv", "/ngordnet/data/ngrams/total_counts.csv");
+		
+		//Act
+		int count = sut.countInYear("airport", 1960);
+		
+		//Assert
+		assertEquals(0, count);
+	}
+	
+	@Test
+	public void countInYear_WordNotInYear_ReturnZero(){
+		//Arrange
+		NGramMap sut = new NGramMap("/ngordnet/data/ngrams/very_short.csv", "/ngordnet/data/ngrams/total_counts.csv");
+		
+		//Act
+		int count = sut.countInYear("invalid", 2005);
+		
+		//Assert
+		assertEquals(0, count);
+	}
+	
+	@Test
+	public void countInYear_GoodWordAndYear_ReturnCount(){
+		//Arrange
+		NGramMap sut = new NGramMap("/ngordnet/data/ngrams/very_short.csv", "/ngordnet/data/ngrams/total_counts.csv");
+		
+		//Act
+		int count = sut.countInYear("wandered", 2005);
+		
+		//Assert
+		assertEquals(83769, count);
+	}
+	
+	@Test
+	public void getRecord_GoodYear_ReturnsData(){
+		//Arrange
+		NGramMap sut = new NGramMap("/ngordnet/data/ngrams/very_short.csv", "/ngordnet/data/ngrams/total_counts.csv");
+		
+		//Act
+		YearlyRecord record = sut.getRecord(2007);
+		
+		//Assert
+		assertEquals(3, record.size());
+	}
+	
+	@Test
+	public void getRecord_NoDataYear_ReturnsEmptyRecord(){
+		//Arrange
+		NGramMap sut = new NGramMap("/ngordnet/data/ngrams/very_short.csv", "/ngordnet/data/ngrams/total_counts.csv");
+		
+		//Act
+		YearlyRecord record = sut.getRecord(1960);
+		
+		//Assert
+		assertEquals(0, record.size());
+	}
+	
+	@Test
+	public void getRecord_RecordModified_OriginalUnaffected(){
+		//Arrange
+		NGramMap sut = new NGramMap("/ngordnet/data/ngrams/very_short.csv", "/ngordnet/data/ngrams/total_counts.csv");
+		
+		//Act
+		YearlyRecord record1 = sut.getRecord(2007);
+		YearlyRecord record2 = sut.getRecord(2007);
+		record1.put("test", 123456);
+		
+		//Assert
+		assertEquals(4, record1.size());
+		assertEquals(3, record2.size());
+	}
+	
+	@Test
+	public void countHistory_TimeRange_CorrectData(){
+		//Arrange
+		NGramMap sut = new NGramMap("/ngordnet/data/ngrams/very_short.csv", "/ngordnet/data/ngrams/total_counts.csv");
+		
+		//Act
+		TimeSeries<Integer> record = sut.countHistory("wandered", 2006, 2009);
+		
+		//Assert
+		assertEquals(4, record.size());
+		assertEquals(87688, record.get(2006).intValue());
+		assertEquals(171015, record.get(2008).intValue());
+		assertEquals(0, record.get(2009).intValue());
+	}
+	
+	@Test
+	public void countHistory_FullHistory_CorrectData(){
+		//Arrange
+		NGramMap sut = new NGramMap("/ngordnet/data/ngrams/very_short.csv", "/ngordnet/data/ngrams/total_counts.csv");
+		
+		//Act
+		TimeSeries<Integer> record = sut.countHistory("airport");
+		
+		//Assert
+		assertEquals(4, record.size());
+		assertEquals(0, record.get(2006).intValue());
+		assertEquals(173294, record.get(2008).intValue());
+	}
+	
+	@Test
+	public void weightHistory_FullHistory_CorrectData(){
+		//Arrange
+		NGramMap sut = new NGramMap("/ngordnet/data/ngrams/very_short.csv", "/ngordnet/data/ngrams/total_counts.csv");
+		
+		//Act
+		TimeSeries<Double> record = sut.weightHistory("airport");
+		
+		//Assert
+		assertEquals(4, record.size());
+		assertEquals(0, record.get(2006).intValue());
+		assertTrue((173294.0/19482936409.0) == record.get(2008));
+	}
+	
+//	2007,16206118071,82969746,155472
+//	2008,19482936409,108811006,206272
+	
+	@Test
+	public void summedWeightHistory_FullHistory_CorrectData(){
+		//Arrange
+		NGramMap sut = new NGramMap("/ngordnet/data/ngrams/very_short.csv", "/ngordnet/data/ngrams/total_counts.csv");
+		Collection<String> words = new ArrayList<String>();
+		words.add("airport");
+		words.add("wandered");
+		
+		//Act
+		TimeSeries<Double> record = sut.summedWeightHistory(words);
+		
+		//Assert
+		assertEquals(4, record.size());
+		assertTrue(((0.0 + 87688.0)/15310495914.0) == record.get(2006));
+		assertTrue(((173294.0 + 171015.0)/19482936409.0) == record.get(2008));
 	}
 }
--- Project1/src/ngordnet/troy/NGramMap.java
+++ Project1/src/ngordnet/troy/NGramMap.java
@@ -10,9 +10,11 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class NGramMap {
-	private HashMap<Integer, YearlyRecord> wordCounts;
+	private TreeMap<Integer, YearlyRecord> wordCounts;
 	private TimeSeries<Long> yearCounts;
 	
     /** Constructs an NGramMap from WORDSFILENAME and COUNTSFILENAME. */
@@ -20,7 +22,7 @@ public class NGramMap {
     	try{
     		
 	    	String sCurrentLine; 
-	    	wordCounts = new HashMap<Integer, YearlyRecord>();
+	    	wordCounts = new TreeMap<Integer, YearlyRecord>();
 	    	yearCounts = new TimeSeries<Long>();
 	    	
 	    	//process wordsFile
@@ -53,7 +55,8 @@ public class NGramMap {
 				Integer year = Integer.decode(pieces[0]);
 				Long wordCount = Long.decode(pieces[1]);
 
-				//per spec we ignore the fourth column
+				//per spec we ignore the third and fourth column
+				//Integer pageCount = Integer.decode(pieces[2]);
 				//Integer volumeCount = Integer.decode(pieces[3]);
 				
 				//add wordcount to yearCounts
@@ -84,44 +87,69 @@ public class NGramMap {
     /** Returns the absolute count of WORD in the given YEAR. If the word
       * did not appear in the given year, return 0. */
     public int countInYear(String word, int year){
-		return year;
-    	
+    	Integer count = 0;
+		YearlyRecord yearCnt = wordCounts.get(year);
+		if(yearCnt != null){
+			count = yearCnt.count(word);
+		}
+    	return count == null ? 0 : count;
     }
 
     /** Returns a defensive copy of the YearlyRecord of YEAR. */
     public YearlyRecord getRecord(int year){
-		return null;
-    	
+    	HashMap<String, Integer> data = new HashMap<String, Integer>();
+    	YearlyRecord record = wordCounts.get(year);
+    	if(record != null)
+    		for(String key: record.words())
+    			data.put(key, record.count(key));
+		return new YearlyRecord(data);	
     }
 
     /** Returns the total number of words recorded in all volumes. */
     public TimeSeries<Long> totalCountHistory(){
-		return null;
-    	
+		return yearCounts;
     }
 
     /** Provides the history of WORD between STARTYEAR and ENDYEAR. */
     public TimeSeries<Integer> countHistory(String word, int startYear, int endYear){
-		return null;
+    	//SortedMap<Integer, YearlyRecord> range = wordCounts.subMap(startYear, true, endYear, true);
+    	TimeSeries<Integer> history = new TimeSeries<Integer>();
+//    	for(Integer key: range.keySet()){
+//    		history.put(key, range.get(key).count(word));
+//    	}
+    	for(int i = startYear; i <= endYear; i++){
+    		YearlyRecord record = wordCounts.get(i);
+    		Integer value = record == null ? 0 : record.count(word);
+    		history.put(i, value);
+    	}
     	
+    	return history;
     }
 
     /** Provides a defensive copy of the history of WORD. */
     public TimeSeries<Integer> countHistory(String word){
-		return null;
+    	return countHistory(word, wordCounts.firstKey(), wordCounts.lastKey());
     	
     }
 
     /** Provides the relative frequency of WORD between STARTYEAR and ENDYEAR. */
     public TimeSeries<Double> weightHistory(String word, int startYear, int endYear){
-		return null;
-    	
+    	TimeSeries<Double> wHistory = new TimeSeries<Double>();
+    	for(int i = startYear; i <= endYear; i++){
+    		YearlyRecord record = wordCounts.get(i);
+    		Integer value = record == null ? 0 : record.count(word);
+    		Long totalCount = yearCounts.get(i);
+    		Double wValue = 0.0;
+    		if(totalCount != null && totalCount != 0)
+    			wValue = value.doubleValue() / totalCount.doubleValue();
+    		wHistory.put(i, wValue);
+    	}
+		return wHistory;
     }
 
     /** Provides the relative frequency of WORD. */
     public TimeSeries<Double> weightHistory(String word){
-		return null;
-    	
+    	return weightHistory(word, wordCounts.firstKey(), wordCounts.lastKey());  	
     }
 
     /** Provides the summed relative frequency of all WORDS between
@@ -129,13 +157,28 @@ public class NGramMap {
       * than throwing an exception. */
     public TimeSeries<Double> summedWeightHistory(Collection<String> words, 
                               int startYear, int endYear){
-    	return null;
+    	TimeSeries<Double> wHistory = new TimeSeries<Double>();
+    	for(int i = startYear; i <= endYear; i++){
+    		YearlyRecord record = wordCounts.get(i);
+    		
+    		Integer value = 0;
+    		for(String word : words){
+    			value += record == null ? 0 : record.count(word);
+    		}
+    		
+    		Long totalCount = yearCounts.get(i);
+    		Double wValue = 0.0;
+    		if(totalCount != null && totalCount != 0)
+    			wValue = value.doubleValue() / totalCount.doubleValue();
+    		wHistory.put(i, wValue);
+    	}
+		return wHistory;
                               	
     }
 
     /** Returns the summed relative frequency of all WORDS. */
     public TimeSeries<Double> summedWeightHistory(Collection<String> words){
-		return null;
+    	return summedWeightHistory(words, wordCounts.firstKey(), wordCounts.lastKey());  	
     	
     }
 
