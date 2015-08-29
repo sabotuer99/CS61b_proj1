a4de4a60c7b7d2c13c4c099123a310bf00860553
troy <sabotuer99@gmail.com>
2015-08-29 01:37:01 -0600

    Added DummyProcessor

---
+++ Project1/src/ngordnet/troy/DummyProcessor.java
@@ -0,0 +1,11 @@
+package ngordnet.troy;
+
+public class DummyProcessor implements YearlyRecordProcessor {
+
+	@Override
+	public double process(YearlyRecord yearlyRecord) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
